8bef517709c91dbb4e7b1a0c65858240920d6c80
Sumira Pathirana <sumiragp@gmail.com>
2024-11-03 11:38:40 +0530

    removed the delay() and updated the code

--- Integration_Testing_-_Green_House.ino
+++ Integration_Testing_-_Green_House.ino
@@ -1,8 +1,6 @@
-
 #define BLYNK_TEMPLATE_ID ""
 #define BLYNK_TEMPLATE_NAME "Green House"
 #define BLYNK_AUTH_TOKEN ""
-
 
 #define BLYNK_PRINT Serial
 
@@ -20,15 +18,23 @@ DFRobot_DHT11 DHT;
 #include <Wire.h>
 BH1750 lightMeter;
 
-
 const int sensor_pin = A0;
 const int led_pin = D4; 
+const int motorPin = D7;
+const int fanPin = D6;
 
+unsigned long motorStartTime = 0;  
+unsigned long fanStartTime = 0;    
+unsigned long lastUpdateTime = 0;    
+bool motorState = LOW;             
+bool fanState = LOW;  
+bool motorOn = false;
+bool fanOn = false; 
 
 void setup() {
   Serial.begin(115200);
   delay(100);
-  // Connect to Blynk
+
   WiFi.begin(ssid, pass);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -36,89 +42,91 @@ void setup() {
   }
   Serial.println("");
   Serial.println("WiFi connected");
-  
+
   Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
 
   Wire.begin();
   lightMeter.begin();
   pinMode(sensor_pin, INPUT);
   pinMode(led_pin, OUTPUT);
- 
-
+  pinMode(fanPin, OUTPUT);
+  pinMode(motorPin,OUTPUT);
 }
 
 void loop() {
-
   Blynk.run();
 
-
-  //DHT 11 segment
   DHT.read(DHT11_PIN);
-  
 
-  //Soil moisture segment
-  float moisture_percentage = ( 100.00 - ( (analogRead(sensor_pin)/1023.00) * 100.00 ) );
-  
-
-  //BH1750 segment
+  float moisture_percentage = (100.00 - ((analogRead(sensor_pin) / 1023.00) * 100.00));
   float lux = lightMeter.readLightLevel();
 
-  if(DHT.temperature > 33){
-
-    Serial.println("High Temperature detected");
-    Blynk.logEvent("temperature_event", String("Green house temperature is greater than 40C!"));
-    digitalWrite(led_pin,LOW);
- 
+  // Temperature monitoring and controlling
+  if(DHT.temperature > 33 && !fanOn) {
+    Serial.println("Starting the fan");
+    fanOn = true;
+    fanState = HIGH;
+    fanStartTime = millis();
+    digitalWrite(fanPin, fanState);
+    Blynk.logEvent("temperature_event", "Green house temperature is greater than 32C!");
   }
-  else{
+  if(fanState == HIGH && millis() - fanStartTime >= 3000) {
+    fanState = LOW;
+    digitalWrite(fanPin, fanState);
+    fanOn = false;
+  }
 
-    digitalWrite(led_pin,HIGH);
-      
-   }
-   if(DHT.humidity > 80){
-
+  if(DHT.humidity > 80) {
     Serial.println("High Humidity detected");
-    Blynk.logEvent("humidity_event", String("Green house humidity is greater than 70%!"));
-    
+    Blynk.logEvent("humidity_event", "Green house humidity is greater than 70%!");
   }
 
-
-   if(moisture_percentage > 50){
-
+  // Soil moisture monitoring and controlling
+  if(moisture_percentage > 50) {
     Serial.println("High soil moisture detected");
-    Blynk.logEvent("soil_moisture_event", String("Green house soil moisture is greater than 50%!"));
- 
-  }
-   else if(moisture_percentage < 10){
-
+    Blynk.logEvent("soil_moisture_event", "Green house soil moisture is greater than 50%!");
+  } else if(moisture_percentage < 10) {
     Serial.println("Low soil moisture detected");
-    Blynk.logEvent("low_soil_moisture_event", String("Green house soil moisture is lower than 10%!"));
-    
-  }
-    if(lux > 900){
-
-    Serial.println("Extreme Sun light detected");
-    Blynk.logEvent("light_intensity_event", String("Green house light intensity is in a critical level!"));
- 
+    Blynk.logEvent("low_soil_moisture_event", "Green house soil moisture is lower than 10%!");
   }
 
-  Serial.print("Green House Temperature is: ");
-  Serial.print(DHT.temperature);
-  Serial.print(" Humidity is: ");
-  Serial.print(DHT.humidity);
-  Serial.print(" | Soil Moisture precentage = ");
-  Serial.print(moisture_percentage);
-  Serial.println("%");
-  Serial.print(" | Light: ");
-  Serial.print(lux);
-  Serial.println(" lx");
+  if(moisture_percentage < 30 && !motorOn) {
+    Serial.println("Starting the motor");
+    motorOn = true;
+    motorState = HIGH;
+    motorStartTime = millis();
+    digitalWrite(motorPin, motorState);
+  }
+  if(motorState == HIGH && millis() - motorStartTime >= 2000) {
+    motorState = LOW;
+    digitalWrite(motorPin, motorState);
+    motorOn = false;
+  }
 
-  Blynk.virtualWrite(V0, DHT.temperature);
-  Blynk.virtualWrite(V1, DHT.humidity);
-  Blynk.virtualWrite(V2, lux);
-  Blynk.virtualWrite(V3, moisture_percentage);
+  if(lux > 900) {
+    Serial.println("Extreme Sunlight detected");
+    Blynk.logEvent("light_intensity_event", "Green house light intensity is in a critical level!");
+  }
 
+  // Print and log data every second
+  if(millis() - lastUpdateTime >= 1000) {
+    lastUpdateTime = millis();  
 
-  delay(1000);
+    Serial.print("Green House Temperature is: ");
+    Serial.print(DHT.temperature);
+    Serial.print(" Humidity is: ");
+    Serial.print(DHT.humidity);
+    Serial.print(" | Soil Moisture percentage = ");
+    Serial.print(moisture_percentage);
+    Serial.println("%");
+    Serial.print(" | Light: ");
+    Serial.print(lux);
+    Serial.println(" lx");
 
+    // Send data to Blynk every second
+    Blynk.virtualWrite(V0, DHT.temperature);
+    Blynk.virtualWrite(V1, DHT.humidity);
+    Blynk.virtualWrite(V2, lux);
+    Blynk.virtualWrite(V3, moisture_percentage);
+  }
 }
